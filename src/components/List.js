e5b94e53074588fdfbcf2cc4b78175da8741ef01
katfastovets <kate@unilimes.com>
2020-08-16 15:43:52 +0300

    add pagination

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Pagination from 'react-bootstrap/Pagination';
 import { ViewerContext } from "../context/ContextWrapper";
+
+const LIST_STEP = 10;
 
 const List = () => {
     const [state, setState] = useContext(ViewerContext);
+
+    const [activePageNumber, setActivePageNumber] = useState(0);
+    const [pageNumbers, setPageNumbers] = useState([]);
+    const [splitList, setSplitList] = useState([]);
 
     const {
         list
@@ -30,12 +37,42 @@ const List = () => {
             })
     }, []);
 
+    useEffect(() => {
+        if (list.length) {
+            const newSplitList = list
+                .slice()
+                .map((item, i, arr) => arr.splice(0, 10))
+                .filter(item => item);
+            setSplitList(newSplitList);
+
+            const pageNums = list.length / LIST_STEP;
+            let items = [];
+            for (let number = 0; number < pageNums; number++) {
+                items.push(
+                    <Pagination.Item
+                        key={number}
+                        active={number === activePageNumber}
+                        onClick={() => setActivePageNumber(number)}
+                    >
+                        {number + 1}
+                    </Pagination.Item>,
+                );
+            }
+            setPageNumbers(items);
+        }
+    }, [list, activePageNumber]);
+
     return (
-        <ListGroup>
-            {list.map(quote =>
-                <ListGroup.Item key={quote.text}>{quote.text}</ListGroup.Item>
-            )}
-        </ListGroup>
+        <>
+            <Pagination>
+                {pageNumbers}
+            </Pagination>
+            <ListGroup>
+                {splitList[activePageNumber]?.map(quote =>
+                    <ListGroup.Item key={quote.text}>{quote.text}</ListGroup.Item>
+                )}
+            </ListGroup>
+        </>
     );
 }
 
